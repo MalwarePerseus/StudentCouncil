27f4cd130f63c1e917d2731530579d4816b4fa75
Deepak Singh <malwareperseus@gmail.com>
2021-08-02 00:18:26 +0530

    Created Basic Nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
+import Link from 'next/link'
+
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/members">
+                <a>Members</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/posts">
+                <a>Work</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>      
+        <Component {...pageProps} />
+      </main>
+    </>
+    )
 }
 
 export default MyApp
