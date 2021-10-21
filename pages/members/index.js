a7e9780c500506e8dc96afe3976789aca6d70ae9
Deepak Singh <malwareperseus@gmail.com>
2021-10-22 01:43:39 +0530

    Completed Members Page

--- pages/members/index.js
+++ pages/members/index.js
@@ -1,59 +1,85 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { getAllHeads, getAllExecHeads, getAllExecMembers } from '../../lib/api'
+import { getAllHeads, getAllExecHeads, getAllExecMems, getAllSportCaps } from '../../lib/api'
 
-export default function Members({ heads, execMems, execHeads }) {
+export default function Members({ heads, execMems, execHeads, sportCaps }) {
   return (
     <div className="members">
       <h1>Council Members</h1>
+
       <div className="grid grid-cols-2">
         {heads.map(head => (
-          <div key={head.slug} className="p-10 bg-purple-600 rounded-3xl my-5 mx-10 hover:bg-purple-700 shadow-2xl">
-            <Image alt={head.name} src={head.profilePictureURL} height="80" width="80" className="rounded-3xl" />
-            <h2>
+          <div key={head.slug} className="py-24 px-40 mx-auto bg-purple-600 rounded-3xl my-5 mx-16 hover:bg-purple-700 shadow-2xl">
+            <div className="-mt-16 pb-8 rounded-full"><Image alt={head.name} src={head.profilePictureURL} height="90" width="90" className="rounded-full" /></div>
+            <h2 className="text-center text-xl font-bold">
               <Link href={head.permalink}>
                 <a>{head.name}</a>
               </Link>
             </h2>
 
-            <Link href={head.permalink}>
-              <a>Go to profile →</a>
-            </Link>
+            <div className="text-lg text-center">
+              <Link href={head.permalink}>
+                <a>Go to profile →</a>
+              </Link>
+            </div>
         </div>    
       ))}
       </div>
       
       <div className="grid grid-cols-3">
         {execHeads.map(execHead => (
-          <div key={execHead.slug} className="p-10 bg-purple-600 rounded-3xl my-5 mx-10 hover:bg-purple-700 shadow-2xl">
-            <Image alt={execHead.name} src={execHead.profilePictureURL} height="80" width="80" className="rounded-3xl" />
-            <h2>
+          <div key={execHead.slug} className="py-24 px-32 mx-auto bg-purple-600 rounded-3xl my-5 mx-16 hover:bg-purple-700 shadow-2xl">
+            <div className="-mt-16 pb-8 rounded-full"><Image alt={execHead.name} src={execHead.profilePictureURL} height="90" width="90" className="rounded-full" /></div>
+            <h2 className="text-center text-xl font-bold">
               <Link href={execHead.permalink}>
                 <a>{execHead.name}</a>
               </Link>
             </h2>
 
-            <Link href={execHead.permalink}>
-              <a>Go to profile →</a>
-            </Link>
+            <div className="text-lg text-center">
+              <Link href={execHead.permalink}>
+                <a>Go to profile →</a>
+              </Link>
+            </div>
         </div>    
       ))}
       </div>
       
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-2">
+        {sportCaps.map(sportCap => (
+          <div key={sportCap.slug} className="py-24 px-36 mx-auto bg-purple-600 rounded-3xl my-5 mx-16 hover:bg-purple-700 shadow-2xl">
+            <div className="-mt-16 pb-8 rounded-full"><Image alt={sportCap.name} src={sportCap.profilePictureURL} height="90" width="90" className="rounded-full" /></div>
+            <h2 className="text-center text-xl font-bold">
+              <Link href={sportCap.permalink}>
+                <a>{sportCap.name}</a>
+              </Link>
+            </h2>
+
+            <div className="text-lg text-center">
+              <Link href={sportCap.permalink}>
+                <a>Go to profile →</a>
+              </Link>
+            </div>
+        </div>    
+      ))}
+      </div>
+
+      <div className="grid grid-cols-4">
         {execMems.map(execMem => (
-          <div key={execMem.slug} className="p-10 bg-purple-600 rounded-3xl my-5 mx-10 hover:bg-purple-700 shadow-2xl">
-            <Image alt={execMem.name} src={execMem.profilePictureURL} height="80" width="80" className="rounded-3xl" />
-            <h2>
+          <div key={execMem.slug} className="py-24 px-20 mx-auto bg-purple-600 rounded-3xl my-5 mx-16 hover:bg-purple-700 shadow-2xl">
+            <div className="-mt-16 pb-8 rounded-full"><Image alt={execMem.name} src={execMem.profilePictureURL} height="90" width="90" className="rounded-full" /></div>
+            <h2 className="text-center text-xl font-bold">
               <Link href={execMem.permalink}>
                 <a>{execMem.name}</a>
               </Link>
             </h2>
 
-            <Link href={execMem.permalink}>
-              <a>Go to profile →</a>
-            </Link>
+            <div className="text-lg text-center">
+              <Link href={execMem.permalink}>
+                <a>Go to profile →</a>
+              </Link>
+            </div>
         </div>    
       ))}
       </div>
@@ -68,6 +94,7 @@ export function getStaticProps() {
       heads: getAllHeads(),
       execMems: getAllExecMems(),
       execHeads: getAllExecHeads(),
+      sportCaps: getAllSportCaps(),
     },
   }
 }
